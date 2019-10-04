8435181743ebae64f1eedc1f46f91d72a8d598b8
Lloyd Risper <lloydrisper@me.com>
2019-10-04 16:04:22 -0400

    Nav mesh

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+
+    [SerializeField] Transform _target;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = _target.position;
+    }
+}
