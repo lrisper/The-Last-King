acf5129df7953746ee421107ebad9ecea21a0b42
Lloyd Risper <lloydrisper@me.com>
2019-10-04 17:23:24 -0400

    Follow Camera

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform _target;
+
+    void Update()
+    {
+        transform.position = _target.position;
+    }
+}
