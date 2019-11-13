516261096257f3e467ced799471948f10f0a3550
Lloyd Risper <lloydrisper@me.com>
2019-11-13 17:59:59 -0500

    Trigger Cinemachine Cut Scene

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        bool _alreadyTriggered = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!_alreadyTriggered && other.tag == "Player")
+            {
+                _alreadyTriggered = true;
+                GetComponent<PlayableDirector>().Play();
+            }
+
+        }
+    }
+}
