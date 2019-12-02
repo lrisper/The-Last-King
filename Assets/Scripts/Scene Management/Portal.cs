f4ac17bf815e4ab478cbb78698a5ef5b1dfdea0e
Lloyd Risper <lloydrisper@me.com>
2019-12-02 01:41:38 -0500

    Scene Loading Portals

---
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int _sceneToLoad = -1;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                SceneManager.LoadScene(_sceneToLoad);
+            }
+            print("Portal Triggered");
+        }
+
+    }
+}
