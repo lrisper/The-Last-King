e7b0d511128cd6d89219760bdfaf88bc2ed4ef2e
Lloyd Risper <lloydrisper@me.com>
2020-01-21 05:15:26 -0500

    Saveable Components And GameObjects

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using RPG.Core;
+using RPG.Saving;
 
 namespace RPG.Movement
 {
 
-    public class Mover : MonoBehaviour, IAction
+    public class Mover : MonoBehaviour, IAction, ISaveable
     {
 
         [SerializeField] Transform _target;
@@ -59,6 +60,17 @@ namespace RPG.Movement
             GetComponent<Animator>().SetFloat("fowardspeed", speed);
         }
 
+        public object CaptureState()
+        {
+            return new SerializableVector3(transform.position);
+        }
 
+        public void RestoreState(object state)
+        {
+            SerializableVector3 position = (SerializableVector3)state;
+            GetComponent<NavMeshAgent>().enabled = false;
+            transform.position = position.ToVector();
+            GetComponent<NavMeshAgent>().enabled = true;
+        }
     }
 }
